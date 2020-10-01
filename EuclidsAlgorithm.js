703bbf3a5aacec0d83e722de6fe111f3ede4df1f
leah4912 <leah29726@gmail.com>
2020-10-01 19:07:15 -0400

    pass test

--- EuclidsAlgorithm.js
+++ EuclidsAlgorithm.js
@@ -1,8 +1,14 @@
 class EuclidsAlgorithm {
     gcd(a, b){
         let divisor = -1;
+        if(a === 0){
+            return b;
+        } else if (b === 0){
+            return a;
+        }else{
+            return this.gcd(b,a%b);
+        }
 
-        return divisor;
     }
 }
 
