10405cc78a6c9bedc8728d5f44975b2b8d3a21c2
leah4912 <leah29726@gmail.com>
2020-10-07 09:29:18 -0400

    tuesdays problem

---
+++ demo.js
@@ -0,0 +1,15 @@
+
+
+function SwapCase(str){
+    var output = "";
+    for(var i = 0; i<str.length; i++){
+        if(str[i] === str[i].toLowerCase()){
+            output += str[i].toUpperCase();
+        }else (str[i == str[i].toUpperCase()])
+         {
+            output += str[i].toLowerCase();
+        }
+    }
+  
+    return outut;
+}
