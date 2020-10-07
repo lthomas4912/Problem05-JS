184cb84b714dafe3f82da0ecc097767cc5d78e96
leah4912 <leah29726@gmail.com>
2020-10-07 09:30:51 -0400

    fixed

--- demo.js
+++ demo.js
@@ -5,11 +5,12 @@ function SwapCase(str){
     for(var i = 0; i<str.length; i++){
         if(str[i] === str[i].toLowerCase()){
             output += str[i].toUpperCase();
-        }else (str[i == str[i].toUpperCase()])
-         {
+        }else if (str[i == str[i].toUpperCase()]){
             output += str[i].toLowerCase();
+        } else{
+            output += str[i];
         }
     }
   
-    return outut;
+    return output;
 }
